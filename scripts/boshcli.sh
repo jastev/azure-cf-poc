55a48cbb8acbe78f90bc35fbed92a2b015f2c03c
root <root@azure-cf-poc.ubo1pi3bmivede5qagldi5x3ab.bx.internal.cloudapp.net>
2018-03-04 18:46:37 +0000

    Bosh-lite install completed successfully on retry; perhaps needed to be root?  Added cfcli.sh

--- scripts/boshcli.sh
+++ scripts/boshcli.sh
@@ -5,8 +5,8 @@ function install_bosh2cli() {
   cd /tmp
   wget -nc https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-2.0.48-linux-amd64
   chmod +x ./bosh-cli-2.0.48-linux-amd64
-  mv ./bosh-cli-2.0.48-linux-amd64 /usr/local/bin
-  ln -s /usr/local/bin/bosh-cli-2.0.48-linux-amd64 /usr/local/bin/bosh2
-  ln -s /usr/local/bin/bosh2 /usr/local/bin/bosh
+  sudo mv ./bosh-cli-2.0.48-linux-amd64 /usr/local/bin
+  sudo ln -s /usr/local/bin/bosh-cli-2.0.48-linux-amd64 /usr/local/bin/bosh2
+  sudo ln -s /usr/local/bin/bosh2 /usr/local/bin/bosh
   popd
 }
