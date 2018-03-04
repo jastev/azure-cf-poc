1ba271c6fda955789f52a3d0b26ecf83b8e7c80c
Jason Stevens <jason.stevens@microsoft.com>
2018-03-04 19:13:23 +0000

    Added stub for install_cloudfoundry, added install_cfcli to setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,4 +9,10 @@ install_bosh2cli
 source boshlite.sh
 install_boshlite /root/bosh-lite/workspace /root/bosh-lite/deployments/virtualbox
 
+source cloudfoundry.sh
+install_cloudfoundry
+
+source cfcli.sh
+install_cfcli
+
 exit 0
