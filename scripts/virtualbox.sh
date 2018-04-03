2dbe11aaf497d32955cd358fed433ec4af2a6df3
Jason Stevens <jason.stevens@microsoft.com>
2018-04-03 16:26:55 -0700

    Remove installation of virtualbox-ext-pack because it requires human intervention

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -4,5 +4,5 @@ function virtualbox_install() {
   echo "deb https://download.virtualbox.org/virtualbox/debian xenial contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
   wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
   sudo apt-get update
-  sudo apt-get install -y virtualbox-5.2 virtualbox-ext-pack
+  sudo apt-get install -y virtualbox-5.2
 }
